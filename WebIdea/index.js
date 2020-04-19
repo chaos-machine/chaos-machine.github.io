11fe96d15ca718efbf978687491c7246d5cc04cc
Drayfus7033 <tudor703344@gmail.com>
2020-04-19 10:45:12 +0200

    tried pop over

--- WebIdea/index.js
+++ WebIdea/index.js
@@ -12,36 +12,41 @@ var letter_spacing = [];
 var letter_spacing_values = [];
 var counter_ls;
 
+if ($('.custom-paddings') != null)
+    var $button_prefab = $('.custom-paddings');
 
-var color_picker;
-var color = "#0433FF";
+var color_picker = $('#picker');
+
+var initial_color = "#0433FF";
+
 var all_caps, title_caps, no_caps;
 var capitalization = "capitalization";
-var corner_radius;
+var corner_radius, color;
 
-var test;
 /*this is where the button is placed in html*/
 let container = document.querySelector('.col-md-9');
 
-$(function () {
 
-    $('#picker').hide(300);
 
-    
+/*enabling pop overs eveywhere*/
+$(document).ready(function () {
+    $('[data-toggle="popover"]').popover();
 });
 
-color_picker = document.getElementById("picker").addEventListener('input', function () {
-    color = this.value;
+
+
+color_picker.on('input', function () {
+    initial_color = this.value;
     if (document.querySelector('.custom-paddings') != null) {
         let button = document.querySelectorAll('.custom-paddings');
         for (var i = 0; i < button.length; i++) {
-            button[i].style.setProperty("background-color", color);
-            button[i].style.setProperty("border-color", color);
+            button[i].style.setProperty("background-color", this.value);
+            button[i].style.setProperty("border-color", this.value);
         }
-
     }
-
 });
+
+    
 
 all_caps = document.getElementById("all-caps").addEventListener('click', function () {
     capitalization = "uppercase";
@@ -127,48 +132,70 @@ padding_vertical = document.getElementById('padding_vertical').addEventListener(
 });
 
 
-function generateDesigns() {
-    console.log("horizontal:" + padding_horizontal_values + "\n" + "vertical: " + padding_vertical_values
-        + "\n" + "letter spacing: " + letter_spacing_values);
 
-    let button_prefab;
+
+
+function generateDesigns() {
+   /// console.log("horizontal:" + padding_horizontal_values + "\n" + "vertical: " + padding_vertical_values
+      ///  + "\n" + "letter spacing: " + letter_spacing_values);
 
     for (var k = 0; k < letter_spacing_values.length; k++) {
         for (var i = 0; i < padding_horizontal_values.length; i++) {
             for (var j = 0; j < padding_vertical_values.length; j++) {
-                button_prefab = document.createElement('button');
-                button_prefab.classList.add("btn-primary", "btn", "space-default", "space-right", "custom-paddings");
+                new_button = document.createElement('button');
+                new_button.classList.add("btn-primary", "btn", "space-default", "space-right", "custom-paddings");
 
-                button_prefab.style.setProperty('--p_left', padding_horizontal_values[i] + "px");
-                button_prefab.style.setProperty('--p_top', padding_vertical_values[j] + "px");
-                button_prefab.style.setProperty('--letter-spacing', letter_spacing_values[k] + "px");
-                button_prefab.style.setProperty('--caps', capitalization);
-                button_prefab.style.setProperty("background-color", color);
-                button_prefab.style.setProperty("border-color", color);
-                button_prefab.id = "button";
+                new_button.style.setProperty('--p_left', padding_horizontal_values[i] + "px");
+                new_button.style.setProperty('--p_top', padding_vertical_values[j] + "px");
+                new_button.style.setProperty('--letter-spacing', letter_spacing_values[k] + "px");
+                new_button.style.setProperty('--caps', capitalization);
+         
+                
+                new_button.style.setProperty("background-color", initial_color);
+                new_button.style.setProperty("border-color", initial_color);
+
+                new_button.id = "button";
 
 
-                button_prefab_text = document.createTextNode('Button');
-                button_prefab.appendChild(button_prefab_text);
-                container.appendChild(button_prefab);
+
+                new_button_text = document.createTextNode('Button');
+                new_button.appendChild(new_button_text);
+                container.appendChild(new_button);
             }
         }
     }
 
 }
 
+
+
+$('*', document.body).on('click','.custom-paddings', function () {
+    console.log(this.id);
+
+
+        var s = '';
+        var o = getComputedStyle(this);
+        for (var i = 0; i < o.length; i++) {
+            if (o[i] == 'padding-left' && (o.getPropertyValue(o[i]) != 0 || o.getPropertyValue(o[i]) != null) )
+                s += o[i] + ':' + o.getPropertyValue(o[i]) + ';';
+        }
+     console.log(s);
+    $('body').popover({
+            
+            selector: '.custom-paddings',
+            content: s,
+            title: "btn properties",
+            placement: 'bottom',
+            trigger: 'focus'
+        });
+
+    });
+
+
+
 function resetOutput() {
 
-    var parent = document.querySelector('.col-md-9');
-
-    while (parent.hasChildNodes() && document.querySelector('.custom-paddings') != null) {
-        let child = document.querySelector('.custom-paddings');
-        parent.removeChild(child);
-
-    }
-
+    $('.custom-paddings').remove();
 }
-
-
 
 
